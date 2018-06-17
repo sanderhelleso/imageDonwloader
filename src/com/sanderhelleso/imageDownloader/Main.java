6e3c7182e593477e730190097a2f9db9cdeb8bd3
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-17 19:30:45 +0200

    can get img urls

--- src/com/sanderhelleso/imageDownloader/Main.java
+++ src/com/sanderhelleso/imageDownloader/Main.java
@@ -1,12 +1,25 @@
 package com.sanderhelleso.imageDownloader;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main extends UrlReader {
 
     public static void main(String[] args) throws IOException {
-	// write your code here
-        String data = UrlReader.readStringFromURL("https://no.wikipedia.org/wiki/Norges_kommuner");
-        System.out.println(data);
+	    // array with lines from url
+        ArrayList<String > data = UrlReader.readStringFromURL("https://no.wikipedia.org/wiki/Norges_kommuner");
+
+        // check for pattern and filter out non matching results
+        for (int i = 0; i < data.size(); i++) {
+            String[] link = data.get(i).split("<img");
+            String imgUrl = link[0].split("href=")[1].split("class")[0];
+            String ext = imgUrl.substring(imgUrl.length() - 5);
+            ext = ext.substring(0, ext.length() - 2);
+
+            // get img url
+            if (ext.equals("svg")) {
+                System.out.println(imgUrl);
+            }
+        }
     }
 }
--- src/com/sanderhelleso/imageDownloader/UrlReader.java
+++ src/com/sanderhelleso/imageDownloader/UrlReader.java
@@ -1,18 +1,27 @@
 package com.sanderhelleso.imageDownloader;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UrlReader {
 
-    public static String readStringFromURL(String requestURL) throws IOException {
-        try (Scanner scanner = new Scanner(new URL(requestURL).openStream(),
-                StandardCharsets.UTF_8.toString()))
-        {
-            scanner.useDelimiter("\\A");
-            return scanner.hasNext() ? scanner.next() : "";
-        }
+    public static ArrayList<String> readStringFromURL(String requestURL) throws IOException {
+        URL wiki = new URL("https://no.wikipedia.org/wiki/Norges_kommuner");
+        BufferedReader in = new BufferedReader(new InputStreamReader(wiki.openStream()));
+
+        String inputLine;
+        ArrayList<String> lines = new ArrayList<>();
+        while ((inputLine = in.readLine()) != null)
+            if (inputLine.contains("<td><a")) {
+                lines.add(inputLine);
+            }
+        in.close();
+
+        return lines;
     }
 }
